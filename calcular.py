98c2d19149168c7912cd865fbdee37bf8a3fc4f0
ACMAX <86621521+axolotl-freckles@users.noreply.github.com>
2024-03-19 17:57:43 -0600

    esfuerzos en reposo

--- calcular.py
+++ calcular.py
@@ -12,6 +12,10 @@ import fourbarforce      as fbf
 
 # from matplotlib import ticker
 
+g = -9.80665
+
+gravity = np.array([0.0, g])
+
 all_tasks = {
 	'geo',
 	'rest_sim',
@@ -112,3 +116,77 @@ if 'rest_sim' in tasks:
 		'F_14y',
 		'T_b',
 	]
+
+	geo_data = pd.read_csv(directorio_parametros+dict_param['geo'])
+	sim_data = pd.read_csv(directorio_parametros+dict_param['sim'])
+
+	for i, name in enumerate(sim_data['name']):
+		idx  = geo_data['name'] == name
+		data = geo_data[idx]
+
+		theta1 = data['t1'][0]
+		theta2 = data['t2'][0]
+		l      = np.array([
+			data['l1'][0],
+			data['l2'][0],
+			data['l3'][0],
+			data['l4'][0],
+		])
+		masses = np.array([
+			1.0,
+			sim_data['m2'][i],
+			sim_data['m3'][i],
+			sim_data['m4'][i],
+		])
+		traction = np.array([0.0, sim_data['normal'][i]])
+
+		print(f'[{i:2d}] calculando "{name}"')
+
+		theta = fba.get_abs_theta(l, theta2, theta1)
+
+		rel_coordinates, abs_coordinates = fbf.to_coordinates(l, theta)
+		wheel_offset   , traction_offset = fbf.sum_rel_vect_to_abs(
+			theta,
+			sim_data['traction_magnitude_offset'][i],
+			sim_data['traction_angle_offset'][i],
+			2,
+			abs_coordinates=abs_coordinates
+		)
+
+		e3_com_rel_vect, e3_com_abs_vect = fbf.sum_rel_vect_to_abs(
+			theta,
+			sim_data['e3_com_magnitude_offset'][i],
+			sim_data['e3_com_angle_offset'][i],
+			2,
+			abs_coordinates=abs_coordinates
+		)
+
+		com_prv, com_nxt = fbf.get_middle_com(rel_coordinates)
+		com_prv[:, 2]    = abs_coordinates[:, 1] - e3_com_abs_vect
+		com_nxt[:, 2]    = abs_coordinates[:, 2] - e3_com_abs_vect
+
+		e3com_traction = traction_offset - e3_com_abs_vect
+
+		geo_mtx = fbf.get_geo_mtx([com_prv, com_nxt], torque_element=1)
+		result_mtx = np.array([
+			0.0 - masses[1]*gravity[0],
+			0.0 - masses[1]*gravity[1],
+			0.0,
+			0.0 - masses[2]*gravity[0] - traction[0],
+			0.0 - masses[2]*gravity[1] - traction[1],
+			0.0 - e3com_traction[0]*traction[1] + e3com_traction[1]*traction[0],
+			0.0 - masses[3]*gravity[0],
+			0.0 - masses[3]*gravity[1],
+			0.0,
+		])
+
+		results = lag.solve(geo_mtx, result_mtx)
+		results[-1] /= 1000
+
+		result_dict = {titulos[i]: results[i] for i in range(len(results))}
+		result_df   = pd.DataFrame(result_dict, index=[name])
+
+		result_dir = f'{directorio_resultados}/SIM_STATIC_{name}.csv'
+		result_df.to_csv(result_dir)
+		print(f'\tGUARDADO EXITOSAMENTE EN: "{result_dir}"')
+		print()
